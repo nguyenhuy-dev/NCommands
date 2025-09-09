c1a21efcf1ce0ca49d1f41e42747ef62e7e72972
nguyenhuy-dev <nguyenhuy.se205@gmail.com>
2025-09-09 10:54:34 +0700

    first commit

---
+++ NFind/FilteredLineSource.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NFind
+{
+    internal class FilteredLineSource : ILineSource
+    {
+        private readonly ILineSource _parent;
+        private readonly Func<Line, bool>? _f;
+
+        public FilteredLineSource(ILineSource parent, Func<Line, bool>? f)
+        {
+            _parent = parent ?? throw new ArgumentNullException(nameof(parent));
+            _f = f;
+        }
+
+        public string FileName => _parent.FileName;
+
+        public void Close()
+        {
+            _parent.Close();
+        }
+
+        public void Open()
+        {
+            _parent.Open();
+        }
+
+        public Line? ReadLine()
+        {
+            if (_f == null) 
+                return _parent.ReadLine();
+
+            var line = _parent.ReadLine();
+            if (line == null)
+                return null;
+            
+            while (line != null && _f(line) == false)
+                line = _parent.ReadLine();
+
+            return line;
+        }
+    }
+}
---
+++ NFindTests/ProgramTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NFind;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NFind.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void BuildOptionsTest()
+        {
+            string[] args = ["/v", "/c", "/n"];
+
+            var options = Program.BuildOptions(args);
+
+            Assert.IsNotNull(options);
+            Assert.IsTrue(options.FindDontConstain);
+            Assert.IsTrue(options.CountMode);
+            Assert.IsTrue(options.IsCaseSensitive);
+        }
+    }
+}
